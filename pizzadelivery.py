af0e84e76aeceb098fa8c3d04340bf7060fda44a
Jay Shivankar <103302425+jayshivankar@users.noreply.github.com>
2022-05-20 16:19:07 +0530

    Update pizzadelivery.py

--- pizzadelivery.py
+++ pizzadelivery.py
@@ -2,6 +2,18 @@ print("Welcome to Python Pizza Deliveries!")
 size = input("What size pizza do you want? S, M, or L ")
 add_pepperoni = input("Do you want pepperoni? Y or N ")
 extra_cheese = input("Do you want extra cheese? Y or N ")
-if size == S :
-    sum+=15
-    elif
+if size == "S":
+    sum += 15
+elif size == "M":
+    sum += 20
+elif size == "L":
+    sum += 25
+    if add_pepperoni == "Y":
+        if size == "S":
+            sum += 2
+        else:
+            sum += 3
+            if extra_cheese == "Y":
+                sum += 2
+            else:
+                print(sum)
